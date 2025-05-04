18b7e7161fdc0c68073987faabb7f8a013bf65c7
Robert Zhang <robert.zhangtianting.zhang@gmail.com>
2025-05-04 19:51:10 +0000

    Fix2 for CFG

--- Teaching_IFT6135-Assignment3-H25-main/q3_cfg_diffusion.py
+++ Teaching_IFT6135-Assignment3-H25-main/q3_cfg_diffusion.py
@@ -19,11 +19,13 @@ class CFGDiffusion():
         super().__init__()
         self.eps_model = eps_model
         self.n_steps = n_steps
+        self.p_uncond = 0.1
+        self.w = 1.0
         
         self.lambda_min = -20
         self.lambda_max = 20
 
-
+        
 
     ### UTILS
     def get_exp_ratio(self, l: torch.Tensor, l_prim: torch.Tensor):
@@ -33,7 +35,10 @@ class CFGDiffusion():
         # TODO: Write function that returns lambda_t for a specific time t. Do not forget that in the paper, lambda is built using u in [0,1]
         # Note: lambda_t must be of shape (batch_size, 1, 1, 1)
         u = t.float() / (self.n_steps)
-        lambda_t = self.lambda_max + (u)*(self.lambda_min - self.lambda_max)
+        b = torch.arctan(torch.exp(-torch.tensor(self.lambda_max) / 2))
+        a = torch.arctan(torch.exp(-torch.tensor(self.lambda_min) / 2)) - b
+        angle = a * u + b
+        lambda_t = -2 * torch.log(torch.tan(angle))
 
         return lambda_t.view(-1, 1, 1, 1)
     
@@ -62,44 +67,38 @@ class CFGDiffusion():
                
     def sigma_q(self, lambda_t: torch.Tensor, lambda_t_prim: torch.Tensor):
         #TODO: Write function that returns variance of the forward process transition distribution q(•|z_l) according to (2)
-        sigmat = self.sigma_lambda(lambda_t)
         sigmatp = self.sigma_lambda(lambda_t_prim)
-
         exp_ratio = self.get_exp_ratio(lambda_t, lambda_t_prim)
-        var_q = sigmatp**2 - exp_ratio**2 * sigmat**2
+
+        var_q = (1 - exp_ratio) * sigmatp**2
+        var_q = torch.sqrt(var_q)
     
-        return var_q.sqrt()
+        return var_q
     
     def sigma_q_x(self, lambda_t: torch.Tensor, lambda_t_prim: torch.Tensor):
         #TODO: Write function that returns variance of the forward process transition distribution q(•|z_l, x) according to (3)
-        alphat = self.alpha_lambda(lambda_t)
-        alphatp = self.alpha_lambda(lambda_t_prim)
-
-        exp_ratio = self.get_exp_ratio(lambda_t, lambda_t_prim)
-        var_q_x = alphatp**2 - exp_ratio**2 * alphat**2
+        var_q_x = self.sigma_q(lambda_t, lambda_t_prim)
     
-        return var_q_x.sqrt()
+        return var_q_x
 
     ### REVERSE SAMPLING
     def mu_p_theta(self, z_lambda_t: torch.Tensor, x: torch.Tensor, lambda_t: torch.Tensor, lambda_t_prim: torch.Tensor):
         #TODO: Write function that returns mean of the forward process transition distribution according to (4)
+        alphat = self.alpha_lambda(lambda_t)
         alphatp = self.alpha_lambda(lambda_t_prim)
         exp_ratio = self.get_exp_ratio(lambda_t, lambda_t_prim)
 
-        first = exp_ratio * z_lambda_t
-        second = alphatp * (1 - exp_ratio**2) * x
+        first = exp_ratio * (alphatp / alphat) * z_lambda_t
+        second = (1 - exp_ratio) * alphatp * x
         mu = first + second
     
         return mu
 
     def var_p_theta(self, lambda_t: torch.Tensor, lambda_t_prim: torch.Tensor, v: float=0.3):
         #TODO: Write function that returns var of the forward process transition distribution according to (4)
-        sigmat = self.sigma_lambda(lambda_t)
-        sigmatp = self.sigma_lambda(lambda_t_prim)
-        exp_ratio = self.get_exp_ratio(lambda_t, lambda_t_prim)
-        first = (1-v) * sigmatp**2
-        second = v * (sigmatp**2 - exp_ratio**2 * sigmat**2)
-        var =  first + second
+        sigmaq = self.sigma_q(lambda_t, lambda_t_prim)
+        sigmaqreverse = self.sigma_q(lambda_t_prim, lambda_t)
+        var = (sigmaq ** (1 - v)) * (sigmaqreverse ** v)
 
         return var
     
@@ -108,10 +107,18 @@ class CFGDiffusion():
         # Note that x_t correspond to x_theta(z_lambda_t)
         if set_seed:
             torch.manual_seed(42)
-        mu = self.mu_p_theta(z_lambda_t, x_t, lambda_t, lambda_t_prim)
+        t = lambda_t.squeeze()
+        alphat = self.alpha_lambda(lambda_t)
+        sigmat = self.sigma_lambda(lambda_t)
+        epscond = (z_lambda_t - alphat * x_t) / sigmat
+        epsuncond = self.eps_model(z_lambda_t, t, None)
+        epsguided = (1 + self.w) * epscond - self.w * epsuncond
+        nxt = (z_lambda_t - sigmat * epsguided) / alphat
+        
+        mu = self.mu_p_theta(z_lambda_t, nxt, lambda_t, lambda_t_prim)
         var = self.var_p_theta(lambda_t, lambda_t_prim)
-        eps = torch.randn_like(z_lambda_t)
-        sample = mu + torch.sqrt(var) * eps
+        noise = torch.randn_like(z_lambda_t)
+        sample = mu + torch.sqrt(var) * noise
     
         return sample 
 
@@ -127,12 +134,16 @@ class CFGDiffusion():
         if noise is None:
             noise = torch.randn_like(x0)
         #TODO: q_sample z
-        lambdat = self.get_lambda(t)
-        zt = self.q_sample(x = x0, lambda_t = lambdat, noise = noise)
+        lambda_t = self.get_lambda(t)
+        zt = self.q_sample(x=x0, lambda_t=lambda_t, noise=noise)
 
         #TODO: compute loss
-        pred_noise = self.eps_model(zt, t, labels)
-        loss = F.mse_loss(pred_noise, noise, reduction = 'mean')
+        uncond_mask = torch.rand(batch_size, device=x0.device) < self.p_uncond
+        labels_adjusted = torch.where(uncond_mask.view(-1, 1), torch.full_like(labels, -1), labels)
+        pred_noise = self.eps_model(zt, t, labels_adjusted)
+        pred_noise_uncond = self.eps_model(zt, t, None)
+        pred_noise = torch.where(uncond_mask.view(-1, 1, 1, 1), pred_noise_uncond, pred_noise)
+        loss = F.mse_loss(pred_noise, noise, reduction='mean')
 
     
         return loss
